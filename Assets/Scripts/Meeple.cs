c4bafbbebc3f71385156d28abeea636c3ce2f91d
Esteban F <36859022+Estebanmister@users.noreply.github.com>
2024-01-04 20:23:35 -0500

    dollhouse scene

--- Assets/Scripts/Meeple.cs
+++ Assets/Scripts/Meeple.cs
@@ -84,6 +84,12 @@ public class Meeple : MonoBehaviour
         city = GameObject.FindGameObjectWithTag("city").GetComponent<City>();
         base_speed = agent.speed;
         base_acceleration = agent.acceleration;
+        meepleSim.fun = UnityEngine.Random.Range(0f,0.5f);
+        meepleSim.bathroom = UnityEngine.Random.Range(0f,0.5f);
+        meepleSim.hunger = UnityEngine.Random.Range(0f,0.5f);
+        meepleSim.social = UnityEngine.Random.Range(0f,0.5f);
+        meepleSim.sleep = UnityEngine.Random.Range(0f,0.5f);
+        meepleSim.hygiene = UnityEngine.Random.Range(0f,0.5f);
     }
 
     void calculateMorale(){
@@ -134,6 +140,9 @@ public class Meeple : MonoBehaviour
                 Interaction interaction = null;
                 foreach(Furniture furniture in place.furnitures){
                     Interaction test = furniture.GetInteraction(needNames[needIndex]);
+                    if(test == null){
+                        continue;
+                    }
                     // begin skill check
                     if(test.skillCheck){
                         // index of what skill we are checking in the loop
@@ -172,46 +181,53 @@ public class Meeple : MonoBehaviour
                         Debug.Log("UNREACHABLE " + furniture.name);
                         continue;
                     }
+                    float v = 0;
                     switch(needNames[needIndex]){
                         // this sucks ass
                         case "hunger":
-                            if(test.hunger > highest_f){
-                                highest_f = test.hunger;
+                            v = ((test.hunger*100)-test.interactionLength)+(Mathf.Pow(needVals[needIndex]*100f,3)*(1f/10000f));
+                            if(v > highest_f){
+                                highest_f = v;
                                 interaction = test;
                                 toInteract = furniture;
                             }
                             break;
                         case "social":
-                            if(test.social > highest_f){
-                                highest_f = test.social;
+                            v = ((test.social*100)-test.interactionLength)+(Mathf.Pow(needVals[needIndex]*100f,3)*(1f/10000f));
+                            if(v > highest_f){
+                                highest_f = v;
                                 interaction = test;
                                 toInteract = furniture;
                             }
                             break;
                         case "sleep":
-                            if(test.sleep > highest_f){
-                                highest_f = test.sleep;
+                            v = ((test.sleep*100)-test.interactionLength)+(Mathf.Pow(needVals[needIndex]*100f,3)*(1f/10000f));
+                            if(v > highest_f){
+                                highest_f =v;
                                 interaction = test;
                                 toInteract = furniture;
                             }
                             break;
                         case "fun":
-                            if(test.fun > highest_f){
-                                highest_f = test.fun;
+                            v = ((test.fun*100)-test.interactionLength)+(Mathf.Pow(needVals[needIndex]*100f,3)*(1f/10000f));
+                            if(v > highest_f){
+                                highest_f = v;
                                 interaction = test;
                                 toInteract = furniture;
                             }
                             break;
                         case "bathroom":
-                            if(test.bathroom > highest_f){
-                                highest_f = test.bathroom;
+                            v = ((test.bathroom*100)-test.interactionLength)+(Mathf.Pow(needVals[needIndex]*100f,3)*(1f/10000f));
+                            if(v > highest_f){
+                                highest_f = v;
                                 interaction = test;
                                 toInteract = furniture;
                             }
                             break;
                         case "hygiene":
-                            if(test.hygiene > highest_f){
-                                highest_f = test.hygiene;
+                            v = ((test.hygiene*100)-test.interactionLength)+(Mathf.Pow(needVals[needIndex]*100f,3)*(1f/100f));
+                            if(v > highest_f){
+                                highest_f = v;
                                 interaction = test;
                                 toInteract = furniture;
                             }
@@ -219,8 +235,15 @@ public class Meeple : MonoBehaviour
                     }
                 }
                 if(toInteract != null){
-                    if(actionQueue.FindIndex(action => action.interaction == interaction) == -1 && currentlyPerforming.interaction != interaction){
-                        actionQueue.Add(new Action{interaction=interaction,timeStarted=city.globalTime,priority=needVals[needIndex],position=toInteract.interactionPoint.position});
+                    if(actionQueue.FindIndex(action => action.interaction == interaction) == -1){
+                        if(currentlyPerforming != null){
+                            if(currentlyPerforming.interaction != interaction){
+                                actionQueue.Add(new Action{interaction=interaction,timeStarted=city.globalTime,priority=needVals[needIndex],position=toInteract.interactionPoint.position});
+                            }
+                        } else {
+                            actionQueue.Add(new Action{interaction=interaction,timeStarted=city.globalTime,priority=needVals[needIndex],position=toInteract.interactionPoint.position});
+                        }
+                        
                     }
                 }
             }
@@ -239,17 +262,17 @@ public class Meeple : MonoBehaviour
         return (val - oldmin) * (newmax - newmin) / (oldmax - oldmin) + newmin;
     }
     bool PerformBasicInteraction(Action action){
-        float time_elapsed = Time.deltaTime*city.timeWarp;
-        action.timeSpentPerforming += time_elapsed;
+        float time_between_frames = Time.deltaTime*city.timeWarp;
+        action.timeSpentPerforming += time_between_frames;
 
         animator.Play(action.interaction.animationToPlay);
 
-        meepleSim.hunger -= action.interaction.hunger * (action.interaction.interactionLength * time_elapsed);
-        meepleSim.social -= action.interaction.social * (action.interaction.interactionLength * time_elapsed);
-        meepleSim.sleep -= action.interaction.sleep * (action.interaction.interactionLength * time_elapsed);
-        meepleSim.hygiene -= action.interaction.hygiene * (action.interaction.interactionLength * time_elapsed);
-        meepleSim.bathroom -= action.interaction.bathroom * (action.interaction.interactionLength * time_elapsed);
-        meepleSim.fun -= action.interaction.fun * (action.interaction.interactionLength * time_elapsed);
+        meepleSim.hunger -= action.interaction.hunger * (time_between_frames/action.interaction.interactionLength);
+        meepleSim.social -= action.interaction.social * (time_between_frames/action.interaction.interactionLength);
+        meepleSim.sleep -= action.interaction.sleep * (time_between_frames/action.interaction.interactionLength);
+        meepleSim.hygiene -= action.interaction.hygiene * (time_between_frames/action.interaction.interactionLength);
+        meepleSim.bathroom -= action.interaction.bathroom * (time_between_frames/action.interaction.interactionLength);
+        meepleSim.fun -= action.interaction.fun * (time_between_frames/action.interaction.interactionLength);
         
         meepleSim.fun = meepleSim.fun < 0 ? 0 : meepleSim.fun;
         meepleSim.bathroom = meepleSim.bathroom < 0 ? 0 : meepleSim.bathroom;
@@ -258,6 +281,14 @@ public class Meeple : MonoBehaviour
         meepleSim.sleep = meepleSim.sleep < 0 ? 0 : meepleSim.sleep;
         meepleSim.hygiene = meepleSim.hygiene < 0 ? 0 : meepleSim.hygiene;
 
+        // sometimes some needs might increase while performing the action, keep them under 1
+        meepleSim.fun = meepleSim.fun > 1 ? 1 : meepleSim.fun;
+        meepleSim.bathroom = meepleSim.bathroom > 1 ? 1 : meepleSim.bathroom;
+        meepleSim.hunger = meepleSim.hunger > 1 ? 1 : meepleSim.hunger;
+        meepleSim.social = meepleSim.social > 1 ? 1 : meepleSim.social;
+        meepleSim.sleep = meepleSim.sleep > 1 ? 1 : meepleSim.sleep;
+        meepleSim.hygiene = meepleSim.hygiene > 1 ? 1 : meepleSim.hygiene;
+
         transform.LookAt(action.position);
         transform.rotation = Quaternion.Euler(0,transform.rotation.eulerAngles.y,0);
         if(action.interaction.skillIncrease){
@@ -265,14 +296,15 @@ public class Meeple : MonoBehaviour
             foreach(string skill in action.interaction.skillsToChange){
                 int index = skills.FindIndex(sk => sk.id == skill);
                 if(index != -1){
-                    skills[index].level += action.interaction.skillLevelstoChange[changeindex] * (action.interaction.interactionLength * time_elapsed);
+                    skills[index].level += action.interaction.skillLevelstoChange[changeindex] * (time_between_frames/action.interaction.interactionLength);
                 } else {
                     skills.Add(new Skill{id = skill, level = 1});
                 }
                 changeindex += 1;
             }
         }
-        if(action.timeSpentPerforming >= action.interaction.interactionLength){
+        if(action.timeSpentPerforming >= action.interaction.interactionLength || 
+            meepleSim.fun == 0 || meepleSim.hunger == 0 || meepleSim.social == 0 || meepleSim.sleep == 0 || meepleSim.hygiene == 0 || meepleSim.bathroom == 0){
             animator.Play("idle");
             foreach(Thought thought in action.interaction.induceThoughts){
                 thoughts.Add(thought.activeThought.copy());
@@ -285,22 +317,32 @@ public class Meeple : MonoBehaviour
     void Update(){
         if(observed){
             // needs increase over time
-            meepleSim.hunger += (meepleSim.hunger<1) ? (Time.deltaTime/100)*city.timeWarp : 0;
-            meepleSim.social += (meepleSim.social<1) ? (Time.deltaTime/100)*city.timeWarp : 0;
-            meepleSim.sleep += (meepleSim.sleep<1) ? (Time.deltaTime/100)*city.timeWarp : 0;
-            meepleSim.hygiene += (meepleSim.hygiene<1) ? (Time.deltaTime/100)*city.timeWarp : 0;
-            meepleSim.bathroom += (meepleSim.bathroom<1) ? (Time.deltaTime/100)*city.timeWarp : 0;
-            meepleSim.fun += (meepleSim.fun<1) ? (Time.deltaTime/100)*city.timeWarp : 0;
+            meepleSim.hunger += (meepleSim.hunger<1) ? (Time.deltaTime/150)*city.timeWarp : 0;
+            meepleSim.social += (meepleSim.social<1) ? (Time.deltaTime/150)*city.timeWarp : 0;
+            meepleSim.sleep += (meepleSim.sleep<1) ? (Time.deltaTime/150)*city.timeWarp : 0;
+            meepleSim.hygiene += (meepleSim.hygiene<1) ? (Time.deltaTime/150)*city.timeWarp : 0;
+            meepleSim.bathroom += (meepleSim.bathroom<1) ? (Time.deltaTime/150)*city.timeWarp : 0;
+            meepleSim.fun += (meepleSim.fun<1) ? (Time.deltaTime/150)*city.timeWarp : 0;
 
             agent.speed = base_speed * city.timeWarp;
             agent.acceleration = base_acceleration * city.timeWarp;
 
             actionQueue = actionQueue.OrderBy(x => x.priority).ToList();
-            if(actionQueue.Count > 0 && !currentlyPerforming.active){
-                currentlyPerforming = actionQueue[UnityEngine.Random.Range(0, actionQueue.Count > 3 ? 2 : actionQueue.Count)];
-                actionQueue.Remove(currentlyPerforming);
-                currentlyPerforming.active = true;
-                agent.destination = currentlyPerforming.position;
+            if(actionQueue.Count > 0){
+                if(currentlyPerforming != null){
+                    if(!currentlyPerforming.active){
+                        currentlyPerforming = actionQueue[UnityEngine.Random.Range(0, actionQueue.Count > 3 ? 2 : actionQueue.Count)];
+                        actionQueue.Remove(currentlyPerforming);
+                        currentlyPerforming.active = true;
+                        agent.destination = currentlyPerforming.position;
+                    }
+                } else {
+                    currentlyPerforming = actionQueue[UnityEngine.Random.Range(0, actionQueue.Count > 3 ? 2 : actionQueue.Count)];
+                    actionQueue.Remove(currentlyPerforming);
+                    currentlyPerforming.active = true;
+                    agent.destination = currentlyPerforming.position;
+                }
+                
             }
             // ladder of ifs...
             // to check if we reached out destination
